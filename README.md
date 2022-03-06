3579a11170d3be33298b335c511bdd7d634d3567
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2022-03-06 22:37:44 +0700

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# BanLai
+
+## ZMP CLI Options
+
+ZMP app created with following options:
+
+```
+{
+  "cwd": "C:\\Users\\MININT-R66V0K6-local\\Documents\\miniapp\\banlai",
+  "newProject": true,
+  "name": "BanLai",
+  "framework": "react-typescript",
+  "template": "blank",
+  "cssPreProcessor": "less",
+  "theming": {
+    "customColor": false,
+    "color": "#007aff",
+    "darkTheme": false,
+    "iconFonts": true,
+    "fillBars": false,
+    "useUiKits": false
+  },
+  "customBuild": false,
+  "includeTailwind": false
+}
+```
+
+## NPM Scripts
+
+* 🔥 `start` - run development server
+* 🙏 `deploy` - deploy mini app for production
---
+++ src/app.ts
@@ -0,0 +1,35 @@
+// Import React and ReactDOM
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Import ZMP
+import ZMP from 'zmp-framework/core/lite-bundle';
+
+// Import ZMP-React Plugin
+import ZMPReact from 'zmp-framework/react';
+
+
+
+// Import ZMP Styles
+import 'zmp-framework/zmp.min.css';
+
+// Import Icons and App Custom Styles
+import './css/icons.css';
+import './css/app.less';
+
+// Import App Component
+import App from './components/app';
+import appConfig from '../app-config.json';
+
+if (!(window as any).APP_CONFIG) {
+  (window as any).APP_CONFIG = appConfig
+} 
+
+// Init ZMP React Plugin
+ZMP.use(ZMPReact)
+
+// Mount React App
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('app'),
+);
