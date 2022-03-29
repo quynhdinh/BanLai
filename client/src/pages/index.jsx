485efee7fdfaf036d0012358f22813c177e16d65
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2022-03-29 09:33:21 +0700

    clean up

--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,39 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
     Page,
-    Navbar,
-    NavTitleLarge,
-    List,
-    ListItem,
     useStore,
-    Card,
+    Searchbar, Box, Icon, Text, Card,
 } from 'zmp-framework/react';
-import AppItems from '../components/app-items';
-import UserCard from '../components/user-card';
 import NavigationBar from '../components/NavigationBar';
+import PostFilter from "../components/HomeMisc/PostFilter";
+import PostDetail from "./post-detail";
 
 const HomePage = () => {
     const user = useStore('user');
+    const [keyword, setKeyword] = useState('')
+
     return (
         <Page name="home" navbarLarge>
             <NavigationBar/>
-            {/* Top Navbar */}
-            <Navbar>
-                <NavTitleLarge>Bán Lại</NavTitleLarge>
-            </Navbar>
-            <List>
-                <ListItem link="/user/">
-                    <UserCard user={user}/>
-                </ListItem>
-            </List>
-
-            {/* Grid apps */}
-            <AppItems/>
-            <List>
-                <ListItem title="Dynamic (Component) Route" link="/dynamic-route/?blog=45&post=125&foo=bar"/>
-                <ListItem title="Default Route (404)" link="/something-that-doesnt-exist"/>
-                <ListItem title="About" link="/about/"/>
-            </List>
+            <Box className="inquiry" px="2">
+                    <div className="flex-1 ">
+                        <Searchbar  className="discount-searchbar" value={keyword}
+                                    onChange={e => setKeyword(e.target.value)} type="text" placeholder="Tìm sản phẩm"
+                                    clearButton onSearchbarClear={() => setKeyword('')}/>
+                        <PostFilter>{"Thiết bị điện tử"}</PostFilter>
+                        <PostFilter>{"Đã qua sử dụng"}</PostFilter>
+                    </div>
+                </Box>
+            {/*</Card>*/}
+            {/*<PostDetail style={{marginTop: 5}}l></PostDetail>*/}
         </Page>
     );
 }
