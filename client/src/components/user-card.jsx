4ec22f1ffe52d49a28851f33e33347352582dade
quynhdv <37500043+quynhdinh@users.noreply.github.com>
2022-03-30 17:30:12 +0700

    stop hard code

--- client/src/components/user-card.jsx
+++ client/src/components/user-card.jsx
@@ -4,8 +4,7 @@ import { Avatar, Title } from 'zmp-framework/react';
 const UserCard = ({ user }) => {
   return (
     <div style={{ display: 'flex', width: '100%' }}>
-        {/*src={'https://inkythuatso.com/uploads/thumbnails/800/2021/09/zalo-logo-inkythuatso-14-15-05-01.jpg'}*/}
-      <Avatar story={user.story} online={user.online} src={'https://inkythuatso.com/uploads/thumbnails/800/2021/09/zalo-logo-inkythuatso-14-15-05-01.jpg'}/>
+      <Avatar story={user.story} online={user.online} src={user.avatar}/>
       <div style={{ marginLeft: 16 }}>
         <Title style={{ marginBottom: 0 }}>{user.displayName}</Title>
         <div>{user.email}</div>
