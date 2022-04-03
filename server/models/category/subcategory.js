708587fff74918999b780dbcdeb07f03bd40e5a6
thanhdat00 <thanhdat02032000@gmail.com>
2022-04-03 14:12:43 +0700

    Delete category,subcategory,city,district

--- server/models/category/subcategory.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var schema = new mongoose.Schema({
-    categoryId: {type: Object, required: true},
-    name: String,
-}, {timestamps: true});
-
-module.exports = mongoose.model('SubCategory', schema);
