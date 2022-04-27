677aeb67569dc1a797426002a89b67a7823c1dda
quynhdv <quynhdv@vng.com.vn>
2022-04-27 11:36:32 +0700

    remove old code
    Bookmark.jsx
    Search.jsx

--- client/src/components/Icons/index.jsx
+++ client/src/components/Icons/index.jsx
@@ -1,19 +1,15 @@
 import ArticleIcon from "./Article"
 import HomeIcon from "./Home"
 import MenuIcon from "./Menu"
-import SearchIcon from "./Search"
 import PlusIcon from "./Plus"
 import Like from "./Like"
-import Bookmark from "./Bookmark"
 import Time from "./Time"
 
 export {
   ArticleIcon,
   HomeIcon,
   MenuIcon,
-  SearchIcon,
   PlusIcon,
   Like,
-  Bookmark,
   Time,
 }
