67042dd0c86ffb566969b460ac76f1849a2cad74
Trần Anh Kiệt <anhkiett524820@gmail.com>
2022-05-10 22:46:29 +0700

    view details - refactor code

--- client/src/pages/post-detail.jsx
+++ client/src/pages/post-detail.jsx
@@ -8,94 +8,114 @@ import {
   Title,
   Icon,
   useStore,
-  Avatar,
   Box,
   Grid,
   GridItem,
-  ListInput,
-  List,
+  zmp,
 } from "zmp-framework/react";
 import "../css/swiper.css";
-import UserCard from "../components/user-card";
 import store from "../store";
 import MessageBox from "../components/message-box";
 import zalo from "../static/icons/Zalo.svg";
 import facebook from "../static/icons/Facebook.svg";
 import messeger from "../static/icons/Messenger.svg";
 import link from "../static/icons/Link.svg";
+import { getReadableTimeGap } from "../util/number";
+import { ViewedItem } from "../components/Categories/";
 
 const linkItems = [zalo, facebook, messeger, link];
 export default () => {
-  const user = useStore("user");
+  const zmproute = zmp.views.main.router.currentRoute;
   const postDetails = useStore("postDetails");
   useEffect(() => {
-    store.dispatch("fetchPostDetail", { id: "626b8291669a242ed89e2ed0" });
-  }, []);
-
+    store.dispatch("fetchPostDetail", { id: zmproute.query?.id });
+  }, [zmproute.query?.id]);
+  console.log(zmproute.query?.id);
   return (
     <Page name="post-detail">
       <Navbar backLink="Back" />
       <Box m={0} style={{ position: "relative" }}></Box>
       <MessageBox isTexted={false} />
-      {/* <Swiper pagination navigation loop>
-        {postDetails[0]?.images.map((item, index) => (
+      <Swiper pagination navigation loop>
+        {postDetails?.[0]?.images.map((item, index) => (
           <SwiperSlide key={index}>
             <img src={item.url} />
           </SwiperSlide>
         ))}
       </Swiper>
-      <Box ml={5}>
+      <Box ml={5} style={{ paddingBottom: 200 }}>
         <Title className="item-name" size={"normal"} bold>
-          {postDetails[0]?.title}
+          {postDetails?.[0]?.title}
         </Title>
-        <div>
-          <Title className="item-price" bold>
-            {postDetails[0]?.price}
-          </Title>
-          <Icon className="liked" zmp="zi-heart" colorTheme={"red"} />
-        </div>
+        <Title className="item-price" bold>
+          {postDetails?.[0]?.price
+            .toString()
+            .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}{" "}
+          đ
+        </Title>
+        <Icon className="liked" zmp="zi-heart" colorTheme={"red"} />
         <Box flexDirection="row" m={0}>
           <PostTag>
-            {postDetails[0]?.district}, {postDetails[0]?.city}
+            {postDetails?.[0]?.district}, {postDetails?.[0]?.city}
           </PostTag>
-          <PostTag>Tin đăng 1 giờ trước</PostTag>
+          <PostTag>
+            Tin đăng {getReadableTimeGap(postDetails?.[0]?.createdAt)}
+          </PostTag>
         </Box>
+        <Description
+          title="Tình trạng sản phẩm"
+          description={postDetails?.[0]?.condition}
+        />
+        {/* {postDetails?.[0]?.productDetails ? (
+          <>
+            <Title size="small" bold>
+              Thông tin chi tiết
+            </Title>
+            <Grid columns={2} noBorder>
+              {postDetails?.[0]?.productDetails.map((item, index) => (
+                <DetailsDescription key={index} title="Màu sắc" description="Đỏ" />
+              ))}
+            </Grid>
+          </>
+        ) : (
+          <></>
+        )} */}
+        <Description
+          title="Mô tả sản phẩm"
+          description={postDetails?.[0]?.description}
+        />
         <Title size="small" bold>
-          Tình trạng sản phẩm
+          Chia sẻ bài đăng
         </Title>
-        <Text size="xsmall"> {postDetails[0]?.condition}</Text>
-        <Title size="small" bold>
-          Thông tin chi tiết
-        </Title>
-        <Grid columns={2} noBorder>
-          <DetailsDescription title="Màu sắc" description="Đỏ" />
-        </Grid>
-        <Title size="small" bold>
-          Mô tả sản phẩm
-        </Title>
-        <Text size="xsmall">{postDetails[0]?.description}</Text>
-
-        
-        <Title bold>Thông tin người bán</Title>
-        <UserCard user={user}></UserCard>
+        <Box ml={0} flexDirection="row" alignItems="left" inline>
+          {linkItems.map((item, index) => (
+            <img key={index} src={item} style={{ marginRight: 12 }} />
+          ))}
+        </Box>
         <Title bold>Sản phẩm tương tự</Title>
-        
-      </Box> */}
-      <Box ml={0} flexDirection="row" alignItems="left" inline>
-        {linkItems.map((item, index) => (
-          <img key={index} src={item} style={{marginRight:8}}/>
-        ))}
+        <ViewedItem />
       </Box>
     </Page>
   );
 };
 
+const Description = ({ title, description }) => (
+  <>
+    <Title size="small" bold>
+      {title}
+    </Title>
+    <Text size="xsmall" style={{ whiteSpace: "break-spaces" }}>
+      {description}
+    </Text>
+  </>
+);
+
 const PostTag = ({ children }) => (
   <Text
     size="xsmall"
-    className="r-round bg-color-nd300 text-color-black"
+    className="r-round bg-color-lg700 text-color-black"
     style={{
-      padding: "2px",
+      padding: "4px",
       marginBottom: "16px",
       marginRight: "4px",
       display: "inline-block",
