b02adccf79cc2c990eced721a9f811b9b6fa9102
quynhdv <quynhdv@vng.com.vn>
2022-05-15 15:55:12 +0700

    refactor: simplify message api code

--- server/routes/message.js
+++ server/routes/message.js
@@ -5,55 +5,48 @@ const router = express.Router();
 const ObjectId = require('mongodb').ObjectId;
 router.use(AuthService.verify);
 
-
-// tra ve ten mat hang, zaloId partner, createdAt, name, avatar
-router.get('/', async (req, res, next) => {
-    try {
-        const zaloId = req.user.zaloId
-        const messages = await db.Messages.find({owner: zaloId})
-        const mess = JSON.parse(JSON.stringify(messages))
-        for(let i = 0; i < mess.length; i++){
-            // User.find({'userID': {$in:array}});
-            const user = await db.Users.find({zaloId: mess[i].partner})
-            const post = await db.Posts.find({_id: ObjectId(mess[i].postId)})
-            const u = JSON.parse(JSON.stringify(user))
-            const p = JSON.parse(JSON.stringify(post))
-            console.log(p)
-            mess[i].picture = u[0].picture
-            mess[i].name = u[0].name
-            mess[i].title = p[0].title
-        }
-        res.send({
-            error: 0,
-            msg: 'Lấy danh sách tin nhắn thành công',
-            data: mess,
-        });
-    } catch (error) {
-        res.send({error: -1, message: 'Unknown exception'});
-        console.log('API-Exception', error);
+router.get('/', async (req, res, _next) => {
+  try {
+    const _messages = await db.Messages.find({owner: req.user.zaloId})
+    const messages = JSON.parse(JSON.stringify(_messages))
+    for (let message of messages) {
+      const user = await db.Users.find({zaloId: message.partner})
+      const post = await db.Posts.find({_id: ObjectId(message.postId)})
+      const u = JSON.parse(JSON.stringify(user))[0]
+      const p = JSON.parse(JSON.stringify(post))[0]
+      message.picture = u.picture
+      message.name = u.name
+      message.title = p.title
     }
+    res.send({
+      error: 0,
+      msg: 'Lấy danh sách tin nhắn thành công',
+      data: messages,
+    });
+  } catch (error) {
+    res.send({error: -1, message: 'Unknown exception'});
+    console.log('API-Exception', error);
+  }
 });
 
-router.post('/', async (req, res, next) => {
-    try {
-        const {
-            owner, partner, type, postId
-        } = req.body
-        const doc = await db.Messages.create({
-            owner: owner,
-            partner: partner,
-            type: type,
-            postId: postId
-        })
-        res.send({
-            error: 0,
-            msg: 'Tạo tin nhắn thành công',
-            data: doc,
-        })
-    } catch (error) {
-        res.send({error: -1, msg: 'Unknown exception'});
-        console.log('API-Exception', error);
-    }
+router.post('/', async (req, res, _next) => {
+  try {
+    const {partner, type, postId} = req.body
+    const msg = await db.Messages.create({
+      owner: req.user.zaloId,
+      partner: partner,
+      type: type,
+      postId: postId
+    })
+    res.send({
+      error: 0,
+      msg: 'Tạo tin nhắn thành công',
+      data: msg,
+    })
+  } catch (error) {
+    res.send({error: -1, msg: 'Unknown exception'});
+    console.log('API-Exception', error);
+  }
 });
 
 module.exports = router;
