f53809e280297f9ac676d9f2a6efe7b5a73752ed
quynhdv <quynhdv@vng.com.vn>
2022-05-19 14:12:26 +0700

    fix: lowercase active

--- client/src/pages/manage-post.jsx
+++ client/src/pages/manage-post.jsx
@@ -41,7 +41,7 @@ export default ({zmproute}) => {
               <Card inset>
                 {userPosts.filter(function (obj) {
                   const o = JSON.parse(JSON.stringify(obj))
-                  return o.status === "Active";
+                  return o.status === "active";
                 }).map((item, index) => (
                   <PostItem key={index}
                             product={{
