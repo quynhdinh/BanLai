366c9f9ba6c29971a9893c8ebd20a3937a92cc73
quynhdv <quynhdv@vng.com.vn>
2022-05-19 12:05:46 +0700

    feat: get user viewed posts

--- server/routes/viewed-post.js
+++ server/routes/viewed-post.js
@@ -2,6 +2,8 @@ const express = require('express');
 const db = require('../models');
 const router = express.Router();
 const AuthService = require("../services/auth-service");
+const {addIsLiked} = require("./helper");
+const {Types} = require("mongoose");
 router.use(AuthService.verify)
 
 router.post('/', async (req, res) => {
@@ -42,5 +44,26 @@ router.post("/count/:postId", async (req, res) => {
     console.log('API-Exception', error);
   }
 });
+
+router.get('/', async (req, res) => {
+  try {
+    const zaloId = req.user.zaloId
+    const filter =
+      (await db.ViewedPostMapping.find({zaloId: zaloId}, {postId: 1, _id: 0}).sort({count: -1}))
+      .map(p => p.postId)
+      .map(p => Types.ObjectId(p))
+    const posts = await db.Posts.find({'_id': {$in: filter}})
+    const response = await addIsLiked(zaloId, posts)
+    res.send({
+      error: 0,
+      msg: 'Lấy danh sách đã xem thành công',
+      data: response,
+    })
+  } catch (error) {
+    res.send({error: -1, msg: 'Unknown exception'});
+    console.log('API-Exception', error);
+  }
+});
+
 
 module.exports = router;
