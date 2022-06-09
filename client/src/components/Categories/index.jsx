2727181c4a0870ab27304e06bc6a816eafdf5d36
quynhdv <quynhdv@vng.com.vn>
2022-06-09 09:44:43 +0700

    init server

--- client/src/components/Categories/index.jsx
+++
@@ -1,9 +0,0 @@
-import Categories from "./Categories"
-import Category from './Category'
-import PostTray from './post-tray'
-
-import './styles.scss'
-
-export {Category}
-export {PostTray}
-export default Categories
--- client/src/components/Input/index.jsx
+++
@@ -1,7 +0,0 @@
-import TextArea from "./text-area";
-import Select from "./select";
-import CustomInput from "./CustomInput";
-
-export default CustomInput;
-export { TextArea };
-export { Select };
