4be540b0a01cc3d76e6b4b3de42435d843c606f5
datltt <datltt@vng.com.vn>
2022-06-13 17:44:58 +0700

    UX: increase refetch time

--- client/src/util/time.js
+++ client/src/util/time.js
@@ -4,7 +4,7 @@ export const isValidCache = (lastFetch) => {
   const current = now();
   const diff = current - lastFetch;
   console.log("[time gap]: " + diff)
-  if (diff < 3 * 60) {
+  if (diff < 10 * 60) {
     console.log("cache valid: [lastFetch]: " + lastFetch + ", now: " + current)
     return true;
   } else {
