7a9348e7b71d6889f9d8c4a023196a72678399c1
Trần Anh Kiệt <anhkiett524820@gmail.com>
2022-06-14 11:59:23 +0700

    category card - UI fix

--- client/src/components/Categories/Category.jsx
+++ client/src/components/Categories/Category.jsx
@@ -89,6 +89,7 @@ const Category = ({ product, border }) => {
                   display: "-webkit-box",
                   WebkitLineClamp: 2,
                   WebkitBoxOrient: "vertical",
+                  wordBreak: "break-word"
                 }}
               >
                 {product.title}
