3bf5942ebea61cccfce04e88c5e8c27903029bea
quynhdv <quynhdv@vng.com.vn>
2022-07-07 15:24:55 +0700

    feat: edit text home

--- website/src/js/cms-preview-templates/home.js
+++ website/src/js/cms-preview-templates/home.js
@@ -52,7 +52,7 @@ export default class PostPreview extends React.Component {
               </div>
 
               <div className="w-60-l ph2-l">
-                <img src="/img/home-about-section.jpg" alt="" className="mb3"/>
+                <img src="/img/all_at_once.gif" alt="" className="mb3"/>
               </div>
             </div>
 
